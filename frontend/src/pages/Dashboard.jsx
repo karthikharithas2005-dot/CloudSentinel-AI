9335b03c246eb440e35769be9909ee4acfde4c71
Karthik <YOUR_GITHUB_EMAIL>
2026-06-10 11:55:21 +0530

    Fix component filename case

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ import {
 
 import Sidebar from "../components/Sidebar";
 import ThreatChart from "../components/ThreatChart";
-import TrendChart from "../components/TrendChart";
+import TrendChart from "../components/TrendChart.jsx";
 
 import "../styles/dashboard.css";
 
